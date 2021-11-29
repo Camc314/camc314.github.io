fbbf93ced4823f2b40c0fc574b57daff9dd70d09
Cameron Clark <cameron.clark@hey.com>
2021-11-29 20:14:03 +0000

    update app to typescript, add tailwind css

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+import { ThemeProvider } from 'next-themes'
+import "../styles/globals.css";
+import 'tailwindcss/tailwind.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
