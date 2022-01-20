a328317ef2220c42e0aab5d3eeb6e95e6f04eb4c
Cameron Clark <cameron.clark@hey.com>
2022-01-20 16:24:18 +0000

    update projects page to use custom backgrounds for each project

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,18 +1,47 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import ContainerWithFooterNav from "../../components/ContainerWithFooterNav";
 
 type project = {
   title: string;
-  description: string;
+  description?: string;
   link: string;
+  projectBackground?: JSX.Element;
+};
+
+const WikitoolsProjectBackground = () => {
+  return (
+    <div className="relative w-full h-full">
+      <Image src="/wikitools-background.png" layout="fill" height={192} />
+      <div className="absolute w-full h-full top-0 left-0 flex justify-center items-center font-mono">
+        <h2 className="text-2xl font-semibold">wikitools</h2>
+      </div>
+    </div>
+  );
+};
+
+const PersonalProjectBackground = () => {
+  return (
+    <div className="bg-white h-full flex flex-col justify-center px-4 shadow-inner">
+      <h3 className="font-bold text-lg">Cameron Clark</h3>
+      <p className="text-sm">Self-taught developer, computer science student, and athlete.</p>
+    </div>
+  );
 };
 
 const projects: project[] = [
-  { title: "Project 1", description: "Project Description", link: "" },
-  { title: "Project 2", description: "Project Description", link: "" },
-  { title: "Project 3", description: "Project Description", link: "" },
-  { title: "Project 4", description: "Project Description", link: "" },
+  {
+    title: "Wikitools",
+    description: "Tools for Processing Data from Wikipedia",
+    link: "/projects/wikitools",
+    projectBackground: <WikitoolsProjectBackground />,
+  },
+  {
+    title: "Personal Website",
+    link: "",
+    projectBackground: <PersonalProjectBackground />,
+  },
 ];
 
 export default function ProjectsPage() {
@@ -33,18 +62,20 @@ export default function ProjectsPage() {
   );
 }
 
-function ProjectItem({ project }: { project: project }) {
+function ProjectItem({ project }: { project: project }): JSX.Element {
   return (
     <Link href={project.link}>
-      <div className="rounded-md bg-gray-400 dark:bg-gray-600 h-48 relative">
-        <span>{project.title}</span>
+      <a className="rounded-md bg-gray-400 dark:bg-gray-600 h-48 relative overflow-hidden">
+        <span>{project.projectBackground || project.title}</span>
         <div className="rounded-md bg-gray-900 bg-opacity-75 opacity-0 hover:opacity-100 transition-all absolute w-full h-full top-0 left-0 flex flex-col justify-end p-4">
           <h3 className="text-3xl text-white font-semibold">{project.title}</h3>
-          <p className="text-base text-white font-light">
-            {project.description}
-          </p>
+          {project.description && (
+            <p className="text-base text-white font-light">
+              {project.description}
+            </p>
+          )}
         </div>
-      </div>
+      </a>
     </Link>
   );
 }
---
+++ pages/projects/wikitools.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerWithFooterNav from "../../components/ContainerWithFooterNav";
+
+export default function WikiTools() {
+  return (
+    <ContainerWithFooterNav pageTitle="Cameron - WikiTools">
+      <div>
+        <h1 className="text-5xl font-bold">Wikitools</h1>
+      </div>
+    </ContainerWithFooterNav>
+  );
+}
